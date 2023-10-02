e3f737109c474321eaea9b5b5e5b4aad4ad4b4fb
Jorly Joseph <jorlymail@gmail.com>
2023-10-02 20:52:33 +0200

    add books screen - select dropdown state

--- src/screens/AddEditBooks/AddEditBooks.jsx
+++ src/screens/AddEditBooks/AddEditBooks.jsx
@@ -1,8 +1,13 @@
+import { useState } from 'react';
 import styles from './AddEditBooks.module.css';
 
 const AddEditBooks = () => {
+  const [language, setLanguage] = useState('DEFAULT');
+  const [category, setCategory] = useState('DEFAULT');
+
   return (
     <div className={styles.container}>
+      <h2>Add books</h2>
       <form>
         <div className="mb-3">
           <label className="form-label">Title</label>
@@ -15,16 +20,32 @@ const AddEditBooks = () => {
         </div>
 
         <label className="form-label">Category</label>
-        <select class="form-select mb-3">
-          <option selected>Please Select</option>
+        <select
+          className="form-select mb-3"
+          onChange={(event) => {
+            setCategory(event.target.value);
+          }}
+          value={category}
+        >
+          <option value="DEFAULT" disabled>
+            Please Select
+          </option>
           <option value="Fiction">Fiction</option>
           <option value="Romance">Romance</option>
           <option value="Horror">Horror</option>
         </select>
 
         <label className="form-label">Language</label>
-        <select class="form-select mb-3">
-          <option selected>Please Select</option>
+        <select
+          className="form-select mb-3"
+          onChange={(event) => {
+            setLanguage(event.target.value);
+          }}
+          value={language}
+        >
+          <option value="DEFAULT" disabled>
+            Please Select
+          </option>
           <option value="English">English</option>
           <option value="German">German</option>
           <option value="Hindi">Hindi</option>
@@ -36,13 +57,13 @@ const AddEditBooks = () => {
           <input type="text" className="form-control" />
         </div>
 
-        <div class="mb-3">
-          <label class="form-label">Description</label>
-          <textarea class="form-control" rows="3" />
+        <div className="mb-3">
+          <label className="form-label">Description</label>
+          <textarea className="form-control" rows="3" />
         </div>
 
         <div className={styles.btnContainer}>
-          <button type="button" class="btn btn-link">
+          <button type="button" className="btn btn-link">
             Cancel
           </button>
 
