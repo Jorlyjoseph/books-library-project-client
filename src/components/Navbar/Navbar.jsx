c2e2310f036bf3669a6f3c02ab529a13ab53a7cc
Jorly Joseph <jorlymail@gmail.com>
2023-10-02 21:43:57 +0200

    navbar link added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import styles from './Navbar.module.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar bg-primary" data-bs-theme="dark">
+      <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
         <div className="container-fluid">
-          <span className="navbar-brand mb-0 h1">Library</span>
+          <Link to="/" className="navbar-brand">
+            <img src="/book-icon.svg" alt="Bootstrap" width="30" height="24" />
+          </Link>
+
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link to="/reader/new" className="nav-link active">
+                  Registration
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/" className="nav-link active">
+                  Search
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/book/new" className="nav-link active">
+                  Add Books
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </div>
