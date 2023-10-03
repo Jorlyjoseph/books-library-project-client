9351835f882330761972f2037f156fce5098969d
Jorly Joseph <jorlymail@gmail.com>
2023-10-03 23:45:01 +0200

    reader search screen markup added

--- src/screens/ReaderSearch/ReaderSearch.jsx
+++ src/screens/ReaderSearch/ReaderSearch.jsx
@@ -1,38 +1,62 @@
+import { useState } from 'react';
 import styles from './ReaderSearch.module.css';
 
 const ReaderSearch = () => {
+  const [query, setQuery] = useState('');
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log(query);
+    // get data using reader search api
+    // save the data to use state
+  };
+
   return (
-    <div className="searchpage">
-      <div>
-        <select className="form-select" aria-label="Default select example">
-          <option selected>Please select</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
-        </select>
-      </div>
-      <div className="{styles.container}">
-        <div className={styles.readerbox}>
-          <div className={styles.info}>
-            <div className={styles.label}>Name</div>
-            <div>John</div>
+    <div className={styles.container}>
+      <form onSubmit={submitHandler}>
+        <div className={styles.searchContainer}>
+          <div className={styles.searchBox}>
+            <div className="input-group input-group-lg">
+              <span className="input-group-text">
+                <i className="bi bi-search"></i>
+              </span>
+
+              <input
+                type="text"
+                className="form-control"
+                onChange={(event) => {
+                  setQuery(event.target.value);
+                }}
+                value={query}
+              />
+            </div>
           </div>
+        </div>
+      </form>
 
-          <div className={styles.bookLogs}>
-            <div className={styles.info}>
+      <div>
+        {/* map the reader box using reader data set in the use state  */}
+        <div className={styles.readerBox}>
+          <div className={styles.leftContainer}>
+            <div className={styles.valuePair}>
+              <div className={styles.label}>Name</div>
+              <div>John</div>
+            </div>
+
+            <div className={styles.valuePair}>
               <div className={styles.label}>Started</div>
-              <div>Date</div>
+              <div>10-12-2003</div>
             </div>
 
-            <div className={styles.info}>
+            <div className={styles.valuePair}>
               <div className={styles.label}>Status</div>
-              <div>Inactive</div>
+              <div>In Active</div>
             </div>
-
+          </div>
+          <div className={styles.rightContainer}>
             <div>
               <span className="badge text-bg-danger">Borrowed</span>
 
-              <span className="badge text-bg-success">Returned</span>
+              {/* <span className="badge text-bg-success">Returned</span> */}
             </div>
 
             <div>
