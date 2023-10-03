3d166fb8c95433ee5bb88e914a5ff2de293af196
Jorly Joseph <jorlymail@gmail.com>
2023-10-03 21:03:54 +0200

    edit book page markup

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import ReaderRegistration from './screens/ReaderRegistraion/ReaderRegistraion';
 import Navbar from './components/Navbar/Navbar';
 import BookHistory from './screens/BookHistory/BookHistory';
 import LoginScreen from './screens/Login/LoginScreen';
-import AddEditBooks from './screens/AddEditBooks/AddEditBooks';
+import AddBooks from './screens/AddBooks/AddBooks';
+import EditBooks from './screens/EditBooks/EditBooks';
 import LentBook from './screens/LentBook/LentBook';
 import ReaderSearch from './screens/ReaderSearch/ReaderSearch';
 import Home from './screens/Home/Home';
@@ -18,7 +19,8 @@ function App() {
         <Route path="/reader/new" element={<ReaderRegistration />} />
         <Route path="/book/:bookId/history" element={<BookHistory />} />
         <Route path="/login" element={<LoginScreen />} />
-        <Route path="/book/new" element={<AddEditBooks />} />
+        <Route path="/book/new" element={<AddBooks />} />
+        <Route path="/book/:bookId/edit" element={<EditBooks />} />
         <Route path="/book/:bookId/lent" element={<LentBook />} />
         <Route path="/reader" element={<ReaderSearch />} />
         <Route path="/" element={<Home />} />
